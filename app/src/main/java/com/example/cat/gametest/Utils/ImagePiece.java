ab527672067e0c184206ccae50c7d55cd2b27f62
兔 艾 <1428245421@qq.com>
2018-04-23 16:01:34 +0800

    Initial commit

---
+++ app/src/main/java/com/example/cat/gametest/Utils/ImagePiece.java
@@ -0,0 +1,45 @@
+package com.example.cat.gametest.Utils;
+
+import android.graphics.Bitmap;
+
+import java.security.Principal;
+
+/**
+ * Created by cat on 2018/4/23.
+ */
+
+public class ImagePiece {
+    @Override
+    public String toString() {
+        return "ImagePiece{" +
+                "index=" + index +
+                ", bitmap=" + bitmap +
+                '}';
+    }
+
+    private  int index;
+    private Bitmap bitmap;
+
+    public int getIndex() {
+        return index;
+    }
+
+    public Bitmap getBitmap() {
+        return bitmap;
+    }
+
+
+    public void setIndex(int index) {
+        this.index = index;
+    }
+
+    public void setBitmap(Bitmap bitmap) {
+        this.bitmap = bitmap;
+    }
+
+
+    public ImagePiece(int index, Bitmap bitmap) {
+        this.index = index;
+        this.bitmap = bitmap;
+    }
+}
---
+++ app/src/main/java/com/example/cat/gametest/Utils/ImageSplitterUtil.java
@@ -0,0 +1,18 @@
+package com.example.cat.gametest.Utils;
+
+import android.graphics.Bitmap;
+
+import java.util.List;
+
+/**
+ * Created by cat on 2018/4/23.
+ */
+
+public class ImageSplitterUtil {
+//    传入bitmap，切割返回
+    public  static List<ImagePiece> splitImage(Bitmap bitmap, int peiece){
+
+        return null;
+
+    }
+}
